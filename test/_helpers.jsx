babcf6c2a8265a1263d1c973436451cb035250eb
Jordan Harband <ljharb@gmail.com>
2018-07-13 15:16:48 -0700

    [Tests] convert from jest to tape, to support older nodes

--- test/_helpers.jsx
+++ test/_helpers.jsx
@@ -1,31 +1,10 @@
-import chai from 'chai';
-import sinonChai from 'sinon-chai';
-import sinon from 'sinon-sandbox';
-import chaiEnzyme from 'chai-enzyme';
-
 import configure from 'enzyme-adapter-react-helper';
 
 configure({ disableLifecycleMethods: true });
-
-chai.use(sinonChai);
-chai.use(chaiEnzyme());
 
 const throwError = (...params) => {
   throw new Error(`A warning or error was logged to the console:\n${params.join('\n')}`);
 };
 
-/* eslint-disable no-console */
-const consoleWarn = console.warn;
-const consoleError = console.error;
-
-beforeEach(() => {
-  console.error = throwError;
-  console.warn = throwError;
-});
-
-afterEach(() => {
-  sinon.restore();
-  console.warn = consoleWarn;
-  console.error = consoleError;
-});
-/* eslint-enable no-console */
+console.error = throwError;
+console.warn = throwError;
--- test/index_test.jsx
+++ test/index_test.jsx
@@ -1,213 +1,182 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { expect } from 'chai';
+import test from 'tape';
 import { shallow } from 'enzyme';
 import casual from 'casual';
+import inspect from 'object-inspect';
 import sloppy from 'prop-types-exact/build/sloppy';
 import createHOC from '../src';
 import { BasicSFC, ProplessSFC } from './fixtures';
 
-describe('createHOC', () => {
-  let hocName;
-  let componentWrapper;
-  let options;
-  beforeEach(() => {
-    hocName = casual.word;
-    options = {
-      factory: true,
-    };
-  });
+test('createHOC', (t) => {
+  const hocName = casual.word;
+  const factoryOptions = {
+    factory: true,
+  };
+  const noopComponentWrapper = Component => () => Component;
 
-  const getHOC = () => createHOC(hocName, componentWrapper, options);
+  t.test('createHOC wrapping', (st) => {
+    st.test('with no-op component wrapper', (s2t) => {
+      const hocFactory = createHOC(
+        hocName,
+        noopComponentWrapper,
+        factoryOptions,
+      );
+      s2t.equal(typeof hocFactory, 'function', 'returns a function');
 
-  describe('createHOC wrapping', () => {
-    describe('with no-op component wrapper', () => {
-      beforeEach(() => {
-        componentWrapper = Component => () => Component;
-      });
+      s2t.test('when passed BasicSFC', (s3t) => {
+        const WrappedComponent = hocFactory()(BasicSFC);
 
-      it('returns a function', () => {
-        expect(getHOC()).to.be.a('function');
-      });
-
-      describe('when passed BasicSFC', () => {
-        const getWrappedComponent = () => getHOC()()(BasicSFC);
-
-        it('returns a React component', () => {
-          const WrappedComponent = getWrappedComponent();
-          expect(React.isValidElement(<WrappedComponent a="a" b="b" c="c" />)).to.equal(true);
-        });
-
-        it('has the same propTypes as BasicSFC', () => {
-          const WrappedComponent = getWrappedComponent();
-          expect(sloppy(WrappedComponent.propTypes)).to.eql(sloppy(BasicSFC.propTypes));
-        });
-
-        it('adds a reasonable displayName to the component', () => {
-          const WrappedComponent = getWrappedComponent();
-          expect(WrappedComponent.displayName).to.equal(`${hocName}(BasicSFC)`);
-        });
-      });
-    });
-
-    describe('with a component wrapper that passes props to the component it wraps', () => {
-      let aParam;
-      let bParam;
-      beforeEach(() => {
-        componentWrapper = (Component, param1, param2) => (
-          function WithAB(props) {
-            return <Component a={param1} b={param2} {...props} />;
-          }
+        s3t.ok(
+          React.isValidElement(<WrappedComponent a="a" b="b" c="c" />),
+          'returns a React component',
         );
-        options.passedProps = ['a', 'b'];
-        aParam = casual.string;
-        bParam = casual.string;
+
+        s3t.deepEqual(
+          sloppy(WrappedComponent.propTypes),
+          sloppy(BasicSFC.propTypes),
+          'has the same propTypes as BasicSFC',
+        );
+
+        s3t.equal(WrappedComponent.displayName, `${hocName}(BasicSFC)`, 'adds a reasonable displayName to the component');
+
+        s3t.end();
       });
 
-      describe('when passed BasicSFC', () => {
-        const getWrappedComponent = () => getHOC()(aParam, bParam)(BasicSFC);
-
-        it('returns a component that wraps the passed component', () => {
-          const WrappedComponent = getWrappedComponent();
-          expect(shallow(<WrappedComponent c="c" />)).to.contain((
-            <BasicSFC a={aParam} b={bParam} c="c" />
-          ));
-        });
-
-        it('removes passed props from prop types of the wrapped component', () => {
-          const WrappedComponent = getWrappedComponent();
-          const { a, b, ...expected } = BasicSFC.propTypes;
-          expect(sloppy(WrappedComponent.propTypes)).to.eql(sloppy(expected));
-        });
-      });
+      s2t.end();
     });
 
-    describe('with a component wrapper that requires new props to be passed', () => {
+    st.test('with a component wrapper that passes props to the component it wraps', (s2t) => {
+      const componentWrapper = (Component, param1, param2) => (
+        function WithAB(props) {
+          return <Component a={param1} b={param2} {...props} />;
+        }
+      );
+      const aParam = casual.string;
+      const bParam = casual.string;
+
+      s2t.test('when passed BasicSFC', (s3t) => {
+        const hocFactory = createHOC(
+          hocName,
+          componentWrapper,
+          { ...factoryOptions, passedProps: ['a', 'b'] },
+        );
+        const WrappedComponent = hocFactory(aParam, bParam)(BasicSFC);
+
+        s3t.ok(
+          shallow(<WrappedComponent c="c" />).contains(<BasicSFC a={aParam} b={bParam} c="c" />),
+          'returns a component that wraps the passed component',
+        );
+
+        const { a, b, ...expected } = BasicSFC.propTypes;
+        s3t.deepEqual(
+          sloppy(WrappedComponent.propTypes),
+          sloppy(expected),
+          'removes passed props from prop types of the wrapped component',
+        );
+
+        s3t.end();
+      });
+
+      s2t.end();
+    });
+
+    st.test('with a component wrapper that requires new props to be passed', (s2t) => {
       const wrapperComponentPropTypes = {
         fooBar: PropTypes.string.isRequired,
       };
 
-      beforeEach(() => {
-        componentWrapper = (Component) => {
-          const WithDerivedAB = ({ fooBar, ...rest }) => (
-            <Component {...rest} />
-          );
+      const componentWrapper = (Component) => {
+        const WithDerivedAB = ({ fooBar, ...rest }) => (
+          <Component {...rest} />
+        );
 
-          WithDerivedAB.propTypes = wrapperComponentPropTypes;
+        WithDerivedAB.propTypes = wrapperComponentPropTypes;
 
-          return WithDerivedAB;
+        return WithDerivedAB;
+      };
+
+      s2t.test('when passed BasicSFC', (s3t) => {
+        const hocFactory = createHOC(
+          hocName,
+          componentWrapper,
+          factoryOptions,
+        );
+        const WrappedComponent = hocFactory()(BasicSFC);
+
+        const fooBar = casual.word;
+        const wrappedComponentProps = {
+          a: casual.word,
+          b: casual.word,
+          c: casual.word,
         };
-      });
-
-      describe('when passed BasicSFC', () => {
-        const getWrappedComponent = () => getHOC()()(BasicSFC);
-
-        it('passes down props as passed', () => {
-          const WrappedComponent = getWrappedComponent();
-          const fooBar = casual.word;
-          const wrappedComponentProps = {
-            a: casual.word,
-            b: casual.word,
-            c: casual.word,
-          };
-          expect(shallow((
+        s3t.ok(
+          shallow((
             <WrappedComponent fooBar={fooBar} {...wrappedComponentProps} />
-          ))).to.contain((
-            <BasicSFC {...wrappedComponentProps} />
-          ));
-        });
+          )).contains(<BasicSFC {...wrappedComponentProps} />),
+          'passes down props as passed',
+        );
 
-        it('adds additonal propTypes to the wrapped component', () => {
-          const WrappedComponent = getWrappedComponent();
-          expect(sloppy(WrappedComponent.propTypes)).to.eql(sloppy({
+        s3t.deepEqual(
+          sloppy(WrappedComponent.propTypes),
+          sloppy({
             ...wrapperComponentPropTypes,
             ...BasicSFC.propTypes,
-          }));
-        });
+          }),
+          'adds additonal propTypes to the wrapped component',
+        );
+
+        s3t.end();
       });
 
-      describe('when passed ProplessSFC', () => {
-        const getWrappedComponent = () => getHOC()()(ProplessSFC);
+      s2t.test('when passed ProplessSFC', (s3t) => {
+        const hocFactory = createHOC(
+          hocName,
+          componentWrapper,
+          factoryOptions,
+        );
+        const WrappedComponent = hocFactory()(ProplessSFC);
 
-        it('does not pass down any props', () => {
-          const WrappedComponent = getWrappedComponent();
-          const fooBar = casual.word;
-          expect(shallow((
-            <WrappedComponent fooBar={fooBar} />
-          ))).to.contain((
-            <ProplessSFC />
-          ));
+        const fooBar = casual.word;
+        s3t.ok(
+          shallow(<WrappedComponent fooBar={fooBar} />).contains(<ProplessSFC />),
+          'does not pass down any props',
+        );
+
+        s3t.deepEqual(
+          sloppy(WrappedComponent.propTypes),
+          sloppy({ ...wrapperComponentPropTypes }),
+          'only has the prop types from the component wrapper',
+        );
+
+        s3t.test('when allowExtraProps is true', (s4t) => {
+          const hocFactoryWithExtraProps = createHOC(
+            hocName,
+            componentWrapper,
+            { ...factoryOptions, allowExtraProps: true },
+          );
+          const WrappedComponentWithExtraProps = hocFactoryWithExtraProps()(ProplessSFC);
+
+          s4t.deepEqual(
+            WrappedComponentWithExtraProps.propTypes,
+            { ...wrapperComponentPropTypes },
+            'does not change the component wrapper prop types',
+          );
+          s4t.end();
         });
 
-        it('only has the prop types from the component wrapper', () => {
-          const WrappedComponent = getWrappedComponent();
-          expect(sloppy(WrappedComponent.propTypes)).to.eql(sloppy({
-            ...wrapperComponentPropTypes,
-          }));
-        });
-
-        describe('when allowExtraProps is true', () => {
-          beforeEach(() => { options.allowExtraProps = true; });
-
-          it('does not change the component wrapper prop types', () => {
-            const WrappedComponent = getWrappedComponent();
-            expect(WrappedComponent.propTypes).to.eql({
-              ...wrapperComponentPropTypes,
-            });
-          });
-        });
+        s3t.end();
       });
+
+      s2t.end();
     });
 
-    describe('with a component wrapper that requires new props to be passed and passes props to the component to be wrapped', () => {
+    st.test('with a component wrapper that requires new props to be passed and passes props to the component to be wrapped', (s2t) => {
       const wrapperComponentPropTypes = {
         fooBar: PropTypes.string.isRequired,
       };
 
-      beforeEach(() => {
-        componentWrapper = (Component) => {
-          const WithDerivedAB = ({ fooBar, ...rest }) => ((
-            <Component a={fooBar.toUpperCase()} b={fooBar.slice(0, 1)} {...rest} />
-          ));
-
-          WithDerivedAB.propTypes = wrapperComponentPropTypes;
-
-          return WithDerivedAB;
-        };
-        options.passedProps = ['a', 'b'];
-      });
-
-      describe('when passed BasicSFC', () => {
-        const getWrappedComponent = () => getHOC()()(BasicSFC);
-
-        it('passes down derived props', () => {
-          const WrappedComponent = getWrappedComponent();
-          const fooBar = casual.word;
-          expect(shallow(<WrappedComponent fooBar={fooBar} c="c" />)).to.contain((
-            <BasicSFC a={fooBar.toUpperCase()} b={fooBar.slice(0, 1)} c="c" />
-          ));
-        });
-
-        it('adds additonal propTypes to the wrapped component', () => {
-          const WrappedComponent = getWrappedComponent();
-          const { a, b, ...expected } = BasicSFC.propTypes;
-          expect(sloppy(WrappedComponent.propTypes)).to.eql(sloppy({
-            ...wrapperComponentPropTypes,
-            ...expected,
-          }));
-        });
-      });
-    });
-  });
-
-  describe('allowExtraProps', () => {
-    const getWrappedComponent = () => getHOC()()(BasicSFC);
-    const wrapperComponentPropTypes = {
-      fooBar: PropTypes.string.isRequired,
-    };
-
-    beforeEach(() => {
-      componentWrapper = (Component) => {
+      const componentWrapper = (Component) => {
         const WithDerivedAB = ({ fooBar, ...rest }) => ((
           <Component a={fooBar.toUpperCase()} b={fooBar.slice(0, 1)} {...rest} />
         ));
@@ -217,109 +186,144 @@ describe('createHOC', () => {
         return WithDerivedAB;
       };
 
-      options.passedProps = ['a', 'b'];
+      const hocFactory = createHOC(
+        hocName,
+        componentWrapper,
+        { ...factoryOptions, passedProps: ['a', 'b'] },
+      );
+      const WrappedComponent = hocFactory()(BasicSFC);
+
+      const fooBar = casual.word;
+      s2t.ok(
+        shallow(<WrappedComponent fooBar={fooBar} c="c" />)
+          .contains(<BasicSFC a={fooBar.toUpperCase()} b={fooBar.slice(0, 1)} c="c" />),
+        'when passed BasicSFC, passes down derived props',
+      );
+
+      const { a, b, ...expected } = BasicSFC.propTypes;
+      s2t.deepEqual(
+        sloppy(WrappedComponent.propTypes),
+        sloppy({
+          ...wrapperComponentPropTypes,
+          ...expected,
+        }),
+        'when passed BasicSFC, adds additonal propTypes to the wrapped component',
+      );
+
+      s2t.end();
     });
 
-    describe('when allowExtraProps is false', () => {
-      it('the resulting wrapped component does not allow extra props', () => {
-        const WrappedComponent = getWrappedComponent();
-        expect(() => shallow(<WrappedComponent fooBar="test" c="test" d="test" />)).to.throw(/unknown props found: d/);
-      });
-    });
-
-    describe('when allowExtraProps is true', () => {
-      beforeEach(() => {
-        options.allowExtraProps = true;
-      });
-
-      it('the resulting wrapped component allows extra props', () => {
-        const WrappedComponent = getWrappedComponent();
-        expect(() => shallow(<WrappedComponent fooBar="test" c="test" d="test" />)).to.not.throw();
-      });
-    });
+    st.end();
   });
 
-  describe('param validation', () => {
-    beforeEach(() => {
-      componentWrapper = Component => () => Component;
-    });
+  t.test('allowExtraProps', (st) => {
+    const wrapperComponentPropTypes = {
+      fooBar: PropTypes.string.isRequired,
+    };
 
-    describe('hocName', () => {
-      it('does not throw when a valid string is passed', () => {
-        expect(() => getHOC()()(BasicSFC)).to.not.throw();
-      });
+    const componentWrapper = (Component) => {
+      const WithDerivedAB = ({ fooBar, ...rest }) => ((
+        <Component a={fooBar.toUpperCase()} b={fooBar.slice(0, 1)} {...rest} />
+      ));
 
-      describe.each([null, undefined, '', 123, true, {}])(
-        'when hocName is `%j`',
-        (name) => {
-          beforeEach(() => {
-            hocName = name;
-          });
+      WithDerivedAB.propTypes = wrapperComponentPropTypes;
 
-          it('Throws an error', () => {
-            expect(() => getHOC()()(BasicSFC)).to.throw(/valid name/);
-          });
-        },
+      return WithDerivedAB;
+    };
+
+    const hocFactory = createHOC(
+      hocName,
+      componentWrapper,
+      { ...factoryOptions, passedProps: ['a', 'b'] },
+    );
+    const WrappedComponent = hocFactory()(BasicSFC);
+
+    st.throws(
+      () => shallow(<WrappedComponent fooBar="test" c="test" d="test" />),
+      /unknown props found: d/,
+      'when allowExtraProps is false, resulting wrapped component does not allow extra props',
+    );
+
+    const hocFactoryWithExtraProps = createHOC(
+      hocName,
+      componentWrapper,
+      { ...factoryOptions, allowExtraProps: true },
+    );
+    const WrappedComponentWithExtraProps = hocFactoryWithExtraProps()(ProplessSFC);
+
+    st.doesNotThrow(
+      () => shallow(<WrappedComponentWithExtraProps fooBar="test" c="test" d="test" />),
+      'when allowExtraProps is true, the resulting wrapped component allows extra props',
+    );
+
+    st.end();
+  });
+
+  t.test('param validation', (st) => {
+    st.doesNotThrow(
+      () => createHOC(hocName, noopComponentWrapper, factoryOptions)()(BasicSFC),
+      'does not throw when a valid string is passed',
+    );
+
+    [null, undefined, '', 123, true, {}].forEach((name) => {
+      st.throws(
+        () => createHOC(name, noopComponentWrapper, factoryOptions)()(BasicSFC),
+        /valid name/,
+        `when hocName is ${inspect(name)}, throws an error`,
       );
     });
 
-    describe('componentAndParamsToComponent', () => {
-      describe('when a valid function with one param', () => {
-        it('does not throw', () => {
-          expect(() => getHOC()()(BasicSFC)).to.not.throw();
-        });
-      });
+    st.test('componentAndParamsToComponent', (s2t) => {
+      s2t.doesNotThrow(
+        () => createHOC(hocName, noopComponentWrapper, factoryOptions)()(BasicSFC),
+        'does not throw when a valid function with one param is passed',
+      );
 
-      describe.each([
+      [
         ['null', null],
         ['a string', casual.word],
         ['an object', {}],
-      ])(
-        'when %s',
-        (_, fn) => {
-          beforeEach(() => { componentWrapper = fn; });
-
-          it('Throws an error', () => {
-            expect(() => getHOC()()(BasicSFC)).to.throw(/function needs to be passed/);
-          });
-        },
-      );
-
-      describe('when a component-returning function with no params', () => {
-        beforeEach(() => { componentWrapper = () => () => null; });
-        it('Throws an error', () => {
-          expect(() => getHOC()()(BasicSFC)).to.throw(/function must take at least one/);
-        });
-      });
-    });
-
-    describe('options', () => {
-      describe('.factory', () => {
-        describe('when true', () => {
-          it('does not throw', () => {
-            expect(() => getHOC()()(BasicSFC)).to.not.throw();
-          });
-        });
-
-        describe('when false', () => {
-          beforeEach(() => { options.factory = false; });
-
-          it('does not throw', () => {
-            expect(() => getHOC()(BasicSFC)).to.not.throw();
-          });
-        });
-
-        describe.each([null, undefined, '', 'foo', 123, {}])(
-          'when `%j`',
-          (factory) => {
-            beforeEach(() => { options.factory = factory; });
-
-            it('Throws an error', () => {
-              expect(() => getHOC()()(BasicSFC)).to.throw(/`factory` option/);
-            });
-          },
+      ].forEach(([desc, invalidFunction]) => {
+        s2t.throws(
+          () => createHOC(hocName, invalidFunction, factoryOptions)()(BasicSFC),
+          /function needs to be passed/,
+          `throws when ${desc} is passed`,
         );
       });
+
+      s2t.throws(
+        () => createHOC(hocName, () => () => null, factoryOptions)()(BasicSFC),
+        /function must take at least one/,
+        'throws when a component-returning function with no params is passed',
+      );
+
+      s2t.end();
     });
+
+    st.test('options', (s2t) => {
+      s2t.doesNotThrow(
+        () => createHOC(hocName, noopComponentWrapper, factoryOptions)()(BasicSFC),
+        'factory: true does not throw',
+      );
+
+      s2t.doesNotThrow(
+        () => createHOC(hocName, noopComponentWrapper, { factory: false })(BasicSFC),
+        'factory: false does not throw',
+      );
+
+      [null, undefined, '', 'foo', 123, {}].forEach((nonBoolean) => {
+        s2t.throws(
+          () => createHOC(hocName, noopComponentWrapper, { factory: nonBoolean })(BasicSFC),
+          /`factory` option/,
+          `factory: non-boolean ${inspect(nonBoolean)} throws`,
+        );
+      });
+
+      s2t.end();
+    });
+
+    st.end();
   });
+
+  t.end();
 });
